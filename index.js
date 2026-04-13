bc2381af57b2cfadafd1d771fe8a3cff51d957e0
atendimentopegma2-code <atendimentopegma2@gmail.com>
2026-04-12 23:04:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+// 🔥 LIBERA ACESSO DO SITE (GitHub Pages)
+app.use(cors());
 
 app.use(express.json());
 
